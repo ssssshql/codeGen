0c64b427adccc39fa20d93a2efe2c76580d66884
GitHub Action <action@github.com>
2025-08-04 09:18:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/ssssshql/codegen/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ssssshql/codegen/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ssssshql.codegen.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ssssshql.codegen.MyBundle
+import com.github.ssssshql.codegen.services.MyProjectService
 import javax.swing.JButton
 
 
